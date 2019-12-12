8a7c5a81d0c9bbdffce915be807c1155ce9c4a33
Andres Perez <andresperezf@yahoo.com>
2019-12-12 14:27:20 -0500

    Before SLDS

--- lwc-services.config.js
+++ lwc-services.config.js
@@ -1,5 +1,15 @@
 // Find the full example of all available configuration options at
 // https://github.com/muenzpraeger/create-lwc-app/blob/master/packages/lwc-services/example/lwc-services.config.js
 module.exports = {
-	resources: [{ from: 'src/resources', to: 'dist/resources' }]
+	resources: [
+		{ from: 'src/resources', to: 'dist/resources' },
+		{
+			from: 'node_modules/@salesforce-ux/design-system/assets',
+			to: 'src/SLDS'
+		},
+		{
+			from: 'node_modules/@salesforce-ux/design-system/assets',
+			to: 'dist/SLDS'
+		}
+	]
 };
