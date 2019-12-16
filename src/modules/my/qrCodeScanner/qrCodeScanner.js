40856d794de0d1ebc6a8836e4c3a6a03822de18b
Andres Perez <andresperezf@yahoo.com>
2019-12-16 07:36:03 -0500

    Made prettier

--- src/modules/my/qrCodeScanner/qrCodeScanner.js
+++ src/modules/my/qrCodeScanner/qrCodeScanner.js
@@ -6,7 +6,7 @@ import { LightningElement, api } from 'lwc';
 import jsQR from 'jsqr'; // npm install jsqr --save
 
 export default class QrCodeScanner extends LightningElement {
-	vars = null;
+	_ui = null;
 	_data = null;
 	_isVisible = false;
 
@@ -21,10 +21,10 @@ export default class QrCodeScanner extends LightningElement {
 
 		if (this._isVisible) {
 			this._startVideo();
-			this._showDiv('divCapture');
+			this._showCaptureData('divCapture');
 		} else {
 			this._stopVideo();
-			this._showDiv('divButton');
+			this._showCaptureData('divData');
 		}
 	}
 
@@ -33,51 +33,46 @@ export default class QrCodeScanner extends LightningElement {
 	}
 
 	renderedCallback() {
-		if (!this.vars) {
-			this.vars = {};
-			this.vars.video = document.createElement('video');
-			this.vars.canvasElement = this.template.querySelector('[data-id=canvas]');
-			this.vars.canvas = this.vars.canvasElement.getContext('2d');
-			this.vars.loadingMessage = this.template.querySelector('[data-id=loadingMessage]');
-			this.vars.outputContainer = this.template.querySelector('[data-id=output]');
-			this.vars.outputMessage = this.template.querySelector('[data-id=outputMessage]');
-			this.vars.outputData = this.template.querySelector('[data-id=outputData]');
+		if (!this._ui) {
+			this._ui = {};
+			this._ui.video = document.createElement('video');
+			this._ui.canvasElement = this.template.querySelector('[data-id=canvas]');
+			this._ui.canvas = this._ui.canvasElement.getContext('2d');
+			this._ui.loadingMessage = this.template.querySelector('[data-id=loadingMessage]');
+			this._ui.outputContainer = this.template.querySelector('[data-id=output]');
+			this._ui.outputMessage = this.template.querySelector('[data-id=outputMessage]');
+			this._ui.outputData = this.template.querySelector('[data-id=outputData]');
+			this._ui.divs = {
+				divCapture: this.template.querySelector('[data-id="divCapture"]'),
+				divData: this.template.querySelector('[data-id="divData"]')
+			};
+			this._ui.loadingMessage.innerText = '⌛ Loading video...';
 		}
 	}
 
 	_tick() {
 		if (this.isVisible) {
-			this.vars.loadingMessage.innerText = '⌛ Loading video...';
-			if (this.vars.video.readyState === this.vars.video.HAVE_ENOUGH_DATA) {
+			if (this._ui.video.readyState === this._ui.video.HAVE_ENOUGH_DATA) {
 				try {
-					console.log('Scanning');
-					this.vars.loadingMessage.hidden = true;
-					this.vars.canvasElement.hidden = false;
-					this.vars.outputContainer.hidden = false;
+					this._ui.loadingMessage.hidden = true;
+					this._ui.canvasElement.hidden = false;
+					this._ui.outputContainer.hidden = false;
 
-					this.vars.canvasElement.height = this.vars.video.videoHeight;
-					this.vars.canvasElement.width = this.vars.video.videoWidth;
-					this.vars.canvas.drawImage(this.vars.video, 0, 0, this.vars.canvasElement.width, this.vars.canvasElement.height);
-					let imageData = this.vars.canvas.getImageData(0, 0, this.vars.canvasElement.width, this.vars.canvasElement.height);
+					this._ui.canvasElement.height = this._ui.video.videoHeight;
+					this._ui.canvasElement.width = this._ui.video.videoWidth;
+					this._ui.canvas.drawImage(this._ui.video, 0, 0, this._ui.canvasElement.width, this._ui.canvasElement.height);
+					let imageData = this._ui.canvas.getImageData(0, 0, this._ui.canvasElement.width, this._ui.canvasElement.height);
 					let code = jsQR(imageData.data, imageData.width, imageData.height, {
 						inversionAttempts: 'dontInvert'
 					});
-					if (code) {
-						this._drawLine(code.location.topLeftCorner, code.location.topRightCorner, '#FF3B58');
-						this._drawLine(code.location.topRightCorner, code.location.bottomRightCorner, '#FF3B58');
-						this._drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, '#FF3B58');
-						this._drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, '#FF3B58');
-						this.vars.outputMessage.hidden = true;
-						this.vars.outputData.parentElement.hidden = false;
-						this.vars.outputData.innerText = code.data;
-						this._reportData(code.data);
-						// } else {
-						// 	this.vars.outputMessage.hidden = false;
-						// 	this.vars.outputData.parentElement.hidden = true;
-					}
+					if (code) this._reportData(code);
 				} catch (e) {
 					console.log(e);
 				}
+			} else {
+				this._ui.loadingMessage.hidden = !true;
+				this._ui.canvasElement.hidden = !false;
+				this._ui.outputContainer.hidden = !false;
 			}
 			window.requestAnimationFrame(() => {
 				this._tick();
@@ -85,18 +80,21 @@ export default class QrCodeScanner extends LightningElement {
 		}
 	}
 
-	_reportData(sData) {
-		const jData = JSON.parse(sData);
-		if (sData !== this._data && jData.copy1 === jData.copy2) {
-			this._data = sData;
+	_reportData(code) {
+		try {
+			const sData = code.data;
+			const jData = JSON.parse(sData);
+
+			this._drawBox(code);
 			this.isVisible = false;
+			this._ui.outputMessage.hidden = true;
+			this._ui.outputData.parentElement.hidden = false;
+			this._ui.outputData.innerHtml = JSON.stringify(jData, null, 2);
 
 			const fetchUrl = `/reportQRCode`;
 			const fetchOptions = {
 				method: 'post',
-				headers: {
-					'Content-Type': 'application/json'
-				},
+				headers: { 'Content-Type': 'application/json' },
 				body: sData
 			};
 			fetch(fetchUrl, fetchOptions)
@@ -104,41 +102,49 @@ export default class QrCodeScanner extends LightningElement {
 					return response.json();
 				})
 				.then(json => {
-					debugger;
 					console.log(JSON.stringify(json));
 				})
 				.catch(e => {
-					throw new Error(e);
+					console.error(e);
 				});
+		} catch (e) {
+			console.error(e);
 		}
 	}
 
-	_drawLine(begin, end, color) {
-		this.vars.canvas.beginPath();
-		this.vars.canvas.moveTo(begin.x, begin.y);
-		this.vars.canvas.lineTo(end.x, end.y);
-		this.vars.canvas.lineWidth = 4;
-		this.vars.canvas.strokeStyle = color;
-		this.vars.canvas.stroke();
+	_drawBox(code) {
+		this._drawLine(code.location.topLeftCorner, code.location.topRightCorner);
+		this._drawLine(code.location.topRightCorner, code.location.bottomRightCorner);
+		this._drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner);
+		this._drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner);
+	}
+
+	_drawLine(begin, end) {
+		let color = '#FF3B58';
+		this._ui.canvas.beginPath();
+		this._ui.canvas.moveTo(begin.x, begin.y);
+		this._ui.canvas.lineTo(end.x, end.y);
+		this._ui.canvas.lineWidth = 4;
+		this._ui.canvas.strokeStyle = color;
+		this._ui.canvas.stroke();
 	}
 
 	_stopVideo() {
 		// Stop all video streams.
-		if (this.vars && this.vars.video && this.vars.video.srcObject) {
-			this.vars.video.srcObject.getVideoTracks().forEach(videoTrack => videoTrack.stop());
+		if (this._ui && this._ui.video && this._ui.video.srcObject) {
+			this._ui.video.srcObject.getVideoTracks().forEach(videoTrack => videoTrack.stop());
 		}
 	}
 
 	_startVideo() {
-		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
 		if (navigator && navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 			// Use facingMode: environment to attemt to get the front camera on phones
 			navigator.mediaDevices
 				.getUserMedia({ video: { facingMode: 'environment' } })
 				.then(stream => {
-					this.vars.video.srcObject = stream;
-					this.vars.video.setAttribute('playsinline', true); // required to tell iOS safari we don't want fullscreen
-					this.vars.video.play();
+					this._ui.video.srcObject = stream;
+					this._ui.video.setAttribute('playsinline', true); // required to tell iOS safari we don't want fullscreen
+					this._ui.video.play();
 					window.requestAnimationFrame(() => {
 						this._tick();
 					});
@@ -151,24 +157,15 @@ export default class QrCodeScanner extends LightningElement {
 		}
 	}
 
-	_showDiv(showDiv) {
-		try {
-			const divs = {
-				divButton: this.template.querySelector('[data-id="divButton"]'),
-				divCapture: this.template.querySelector('[data-id="divCapture"]')
-			};
-
-			Object.keys(divs).forEach(key => {
-				if (key === showDiv) {
-					divs[key].classList.add('slds-show');
-					divs[key].classList.remove('slds-hide');
-				} else {
-					divs[key].classList.remove('slds-show');
-					divs[key].classList.add('slds-hide');
-				}
-			});
-		} catch (e) {
-			console.log(e);
+	_showCaptureData(showDiv) {
+		if (this._ui && this._ui.divs) {
+			try {
+				Object.keys(this._ui.divs).forEach(key => {
+					this._ui.divs[key].hidden = key !== showDiv;
+				});
+			} catch (e) {
+				console.log(e);
+			}
 		}
 	}
 }
